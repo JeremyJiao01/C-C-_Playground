d204f8dc08bf6d51f86a740724773b63c76dfb99
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-01-25 15:32:21 +0800

    Added #4

--- LeetCode/BinarySearch/BinarySearch_Insert.c
+++ LeetCode/BinarySearch/BinarySearch_Insert.c
@@ -322,3 +322,47 @@ int findInint(int target, int* nums, int numsSize) {
     return left;
 }
 
+
+// # 4 寻找两个正序数组的中位数
+// 求中位数 = 求第 k 小的数
+// 理想情况：两数组中无重复元素
+// 则查找第 k 小的数即：
+// 1. A数组中的k/2（向下取整）+ B数组中的k/2（向下取整）
+//    若A[k/2] < B[k/2]则A[0]~A[k/2]都舍弃
+// 2. 由于排除了k/2个，此时k = K - K/2，再重复第一步中的操作
+// 3. 当一个数组中所有元素都被舍弃，则直接在另一数组中找值即可 
+
+int min (int a, int b) {
+    if (a>b)
+        return b;
+    else
+        return a;
+}
+
+// 求第k个值
+int getKnum(int* nums1, int start1, int end1, int* nums2, int start2, int end2, int k){
+    int len1 = end1 - start1 + 1;
+    int len2 = end2 - start2 + 1;
+    if(len1 > len2) return getKnum(nums2, start2, end2, nums1, start1, end1, k); // 始终保证nums1的数组小于nums2的数组
+    if(len1 == 0) return nums2[start2 + k - 1];
+    if(k == 1) return min(nums1[start1], nums2[start2]);
+    
+    int i = start1 + min(len1, k/2) - 1;
+    int j = start2 + min(len2, k/2) - 1;
+
+    if(nums1[i] > nums2[j]){
+	// 通过判断舍弃数组前k个元素
+        return getKnum(nums1, start1, end1, nums2, j + 1, end2, k - (j-start2 + 1));
+    }else{
+        return getKnum(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+    }
+}
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int n = nums1Size;
+    int m = nums2Size;
+    int left = (n + m + 1) / 2;
+    int right = (n + m + 2) / 2;
+    return (getKnum(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKnum(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5; 
+    // 取平均数，因为可能出现奇数和偶数
+}
