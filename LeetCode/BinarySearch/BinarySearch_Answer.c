a5a316e93024bc253daf0b1948974fcfb5fcb6a9
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-01-26 09:55:10 +0800

    Added #287

--- LeetCode/BinarySearch/BinarySearch_Answer.c
+++ LeetCode/BinarySearch/BinarySearch_Answer.c
@@ -20,25 +20,33 @@ int mySqrt(int x) {
 }
 
 // # 287 寻找重复数
-
+// 思路：
+// 对于一个无序且有重复数字的数组
+// 要找到其重复数字，我们需要计算每个数字的cnt值
+// cnt[i] = nums[1 : n] <= i 的个数
+// 即：当i <= target时，nums[1 : target - 1] 里的每个cnt[i] <= i
+//     因为每个在target之前没有数字重复
+//     使用这种方法就不用考虑查询的数组是否有序
+//     因为判别依据是使用数组下标而不是数组内元素
 int findDuplicate(int* nums, int numsSize){
-	int left = 0;
+	int left = 1; // 起始下标不需要为0，因为是通过下标判断
 	int right = numsSize - 1;
 	int ans = -1;
 	while(left < right){
-		int mid = (left + right + 1) >> 1;
+		int mid = (left + right) >> 1;
 		int cnt = 0;
 		int i = 0;
 		for(int i = 0; i < n; i++){
 			if(nums[i] <= mid){
 				cnt++;
+				// 计算当前下标的cnt值
 			}		
 		}
-		if(cnt > mid){
+		if(cnt <= mid){ // 处于target之前
+			left = mid + 1;
+		}else{
 			right = mid - 1;
 			ans = mid;
-		}else{
-			left = mid;
 		}
 	}
 	return ans;
