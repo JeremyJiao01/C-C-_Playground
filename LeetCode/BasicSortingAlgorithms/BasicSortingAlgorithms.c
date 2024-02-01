4990b032a5e4e888ed5a64f7770b961030dac5d0
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-02-01 11:29:07 +0800

    Added InsertSort

--- LeetCode/BasicSortingAlgorithms/BasicSortingAlgorithms.c
+++ LeetCode/BasicSortingAlgorithms/BasicSortingAlgorithms.c
@@ -18,3 +18,35 @@ int* sortArray(int* nums, int numsSize, int* returnSize) {
     }
     return nums;
 }
+
+// 插入排序
+
+// 插入排序方法一
+// 不断调换位置以达到有序
+int* insertSorted1(int* nums, int numsSize){
+    for(int i = 0; i < numsSize; i++){
+        for(int j = i; j > 0; j--){
+            if(nums[j] < nums[j-1]){
+                swap(nums, j-1, j);
+            }else{
+                break;
+            }
+        }
+    }
+    return nums;
+}
+
+// 插入排序方法二
+// 先将有序数组对调出一个空位，再将值插入
+int* insertSorted2(int* nums, int numsSize){
+    for(int i = 1; i < numsSize; i++){
+        int temp = nums[i];
+        int j = i;
+        while(j > 0 && nums[j - 1] > temp){
+            nums[j] = nums[j - 1];
+            j--;
+        }
+        nums[j] = temp;
+    }
+    return  nums;
+}
