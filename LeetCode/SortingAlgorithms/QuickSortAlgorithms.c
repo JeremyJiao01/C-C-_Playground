bd74c7f979376a13a9c594a6b97dde828a08dba1
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-02-17 16:22:34 +0800

    Added #80

--- LeetCode/SortingAlgorithms/QuickSortAlgorithms.c
+++ LeetCode/SortingAlgorithms/QuickSortAlgorithms.c
@@ -135,3 +135,20 @@ int removeDuplicates(int* nums, int numsSize) {
     }
     return i + 1;
 }
+
+// # 80 删除有序数组中多余两个的重复项
+int removeDuplicates(int* nums, int numsSize) {
+    if (numsSize <= 2) {
+        return numsSize;
+    }
+    int slow = 2, fast = 2;
+    while (fast < numsSize) {
+        if (nums[slow - 2] != nums[fast]) {
+            nums[slow] = nums[fast];
+            ++slow;
+        }
+        ++fast;
+    }
+    return slow;
+}
+
--- LeetCode/Tools/Tools.c
+++ LeetCode/Tools/Tools.c
@@ -3,19 +3,56 @@
 //
 
 #include "Func.h"
-
+int removeDuplicates(int* nums, int numsSize) {
+    int i = 0;
+    int j = 1;
+    int tmp = 1;
+    while(j <= numsSize - 1){
+        if(nums[i] != nums[j]){
+            tmp = 1;
+            if(j - i > 1){
+                nums[i + 1] = nums[j];
+                j++;
+            }else{
+                j++;
+            }
+            i++;
+        }else{
+            tmp++;
+            if(tmp <= 2){
+                if(j - i > 1) {
+                    nums[i + 1] = nums[j];
+                }
+                i++;
+                j++;
+            }
+            if(tmp > 2){
+                while(j <= numsSize - 1 && nums[i] == nums[j]){
+                    j++;
+                }
+                if(j <= numsSize - 1){
+                    i++;
+                    nums[i] = nums[j];
+                    j++;
+                    tmp = 1;
+                }
+            }
+        }
+    }
+    return i + 1;
+}
 
 int main(){
-    srand(time(NULL));
-
-    int* randomArr = generateRandomArray(genNum);
-    Display(randomArr, genNum);
-    testSortTime(randomArr, genNum, quickSortArr);
-//    int record[4] = {-1, -1};
-//    int* nums = countSmaller(&randomArr, genNum);
-//    for(int i = 0; i < genNum; i++){
-//        printf("%d ", nums[i]);
-//    }
-
+//    srand(time(NULL));
+//
+//    int* randomArr = generateRandomArray(genNum);
+//    Display(randomArr, genNum);
+//    testSortTime(randomArr, genNum, quickSortArr);
+    int record[] = {1, 1, 1};
+    int nums = removeDuplicates(&record, 3);
+    for(int i = 0; i < nums; i++){
+        printf("%d ", record[i]);
+    }
+    printf("%d", nums);
     return 0;
 }
