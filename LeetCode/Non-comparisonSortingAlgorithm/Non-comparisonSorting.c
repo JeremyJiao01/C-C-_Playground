c3c9f554414a701497c2f377d6e5eeb925037a8e
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-02-20 17:23:28 +0800

    radixSort

--- LeetCode/Non-comparisonSortingAlgorithm/Non-comparisonSorting.c
+++ LeetCode/Non-comparisonSortingAlgorithm/Non-comparisonSorting.c
@@ -32,3 +32,65 @@ void sort(int* nums, int numsSize){
 		count[numsCopy[i]]--;
 	}
 }
+
+// 低位优先的基数排序
+// 按照个、十、百的顺序对数值进行排序操作
+// 低位优先的基数排序，越高位的排序是放在后面进行的，
+// 在高位相同的情况下，需要比较次高位，而次高位在之前的排序中已经排好序。
+// 每一趟基于关键字的排序 必须使用稳定排序
+
+int getMaxLen(int num){
+	int maxlen = 0;
+	while(num > 0){
+		num /= 10;
+		maxlen++;
+	}
+	return maxlen;
+}
+
+void countingSort(int* nums, int* tmp, int divisor, int len, int* count){
+	// 内层循环将数组从头遍历一遍
+	for(int j = 0; j < len; j++){
+		// 计算指定数位上的数是几
+		int remainder = (nums[j] / divisor) % 10;
+		count[remainder]++;
+	}
+	for(int j = 1; j< 10; j++){
+		count[j] += count[j - 1];
+	}
+	for(int j = len - 1; j >= 0; j--){
+		int remainder = (nums[j] / divisor) % 10;
+		int index = count[remainder] - 1;
+		tmp[index] = nums[j];
+		count[remainder]--;
+	}
+	for(int j = 0; j < 10; j++){
+		count[j] = 0;
+	}
+}
+
+void radixSort(int* nums, int numsSize){
+	int max = nums[0];
+	for(int i = 0; i < len; i++){
+		if(nums[i] > max){
+			max = nums[i];
+		}
+	}
+	// 计算出最大的数字有几位
+	// 这个数值决定了我们要将整个数组看几遍
+	int maxlen = getMaxLen(max);
+	// 每一趟都适用计数排序
+	int* count = (int*)malloc(sizeof(int) * 10);
+	int* tmp = (int*)malloc(sizeof(int) * numsSize);
+	int divisor = 1;
+	for(int i = 0; i < maxlen; i++){
+		// 每一步都使用计数排序
+		countingSort(nums, tmp, divisor, len, count);
+		int* numsCopy = (int*)malloc(sizeof(int) * numsSize);
+		for(int i = 0; i < numsSize; i++){
+				numsCopy[i] = nums[i];
+		}
+		divisor *= 10;
+	}
+} 
+
