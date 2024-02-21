9e06a7e4756ada3c5601809e5bd8e01d4e04728d
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-02-21 18:23:52 +0800

    Added BucketSort

--- LeetCode/Non-comparisonSortingAlgorithm/Non-comparisonSorting.c
+++ LeetCode/Non-comparisonSortingAlgorithm/Non-comparisonSorting.c
@@ -94,3 +94,73 @@ void radixSort(int* nums, int numsSize){
 	}
 } 
 
+// 桶排序
+// 桶：可以容纳一个范围内的数
+// 1. 先将数组中的数归类在各自对应的桶中
+// 2. 每个桶内部进行排序
+// 3. 按顺序取出所有桶的数，即得到有序数组
+// 以下是一些设定桶排序区间范围的注意事项：
+// 		每个桶的区间范围应该尽可能均匀，以便每个桶中的元素数量大致相同。
+// 		如果待排序数据的元素分布不均匀，可以采用哈希函数或自定义划分等方法来提高桶排序的效率。
+// 		桶排序的时间复杂度与桶的数量成正比，因此桶的数量不宜过多。
+
+void insertSort(int* nums, int numsSize){
+	for(int i = 0; i <= numsSize; i++){
+		int tmp = nums[i];
+		int j = i;
+		while(j > 0 && nums[j - 1] > tmp){
+			nums[j] = nums[j - 1];
+			j--;
+		}
+		nums[j] = tmp;
+	}
+}
+
+void bucketSort(int* nums, int numsSize){
+	int max = nums[0];
+	// 找到数组中的最大值，以确定计数数组的长度
+	for(int i = 0; i < numsSize; i++){
+		if(nums[i] > max){
+			max = nums[i];
+		}
+	}
+	// 计算数组中最大的数的位数
+	// 这个决定了需要几个桶
+	int maxLen = getMaxLen(max);
+	// 步长，步长决定了每个桶容纳数的范围区间
+	int step = 1000;
+	if(maxLen < 5){
+		step = (int)pow(10, maxLen - 1);
+	}
+
+	int bucketLen = max / step + 1; // 确定桶的数量
+	int tmp[bucketLen][numsSize] = {0}; 
+	// 因为不确定每个桶内的元素数量，所以都设置为numsSize
+	int next[bucketLen] = {0}; 
+
+	// 分桶
+	for(int i = 0; i < numsSize; i++){
+		// 找到索引
+		int bucketIndex = nums[i] / step;
+		tmp[bucketIndex][next[bucketIndex]] = nums[i];
+		// 找到第几个桶，然后这个将元素存在这个桶的位置
+		next[bucketIndex]++;
+		// 更新这个桶内元素的数量
+	}
+
+	// 对每个桶进行插入排序
+	for(int i = 0; i < bucketLen; i++){
+		insertSort(tmp[i], next[i] - 1);
+	}
+
+	// 将桶内元素依次取出
+	int index = 0;
+	for(int i = 0; i < bucketLen; i++){
+		int curLen = next[i];
+		for(int j = 0; j < curLen; j++){
+			nums[index] = tmp[i][j];
+			index++;
+		}
+	}
+}
+
--- LeetCode/Tools/Tools.c
+++ LeetCode/Tools/Tools.c
@@ -3,44 +3,7 @@
 //
 
 #include "Func.h"
-int removeDuplicates(int* nums, int numsSize) {
-    int i = 0;
-    int j = 1;
-    int tmp = 1;
-    while(j <= numsSize - 1){
-        if(nums[i] != nums[j]){
-            tmp = 1;
-            if(j - i > 1){
-                nums[i + 1] = nums[j];
-                j++;
-            }else{
-                j++;
-            }
-            i++;
-        }else{
-            tmp++;
-            if(tmp <= 2){
-                if(j - i > 1) {
-                    nums[i + 1] = nums[j];
-                }
-                i++;
-                j++;
-            }
-            if(tmp > 2){
-                while(j <= numsSize - 1 && nums[i] == nums[j]){
-                    j++;
-                }
-                if(j <= numsSize - 1){
-                    i++;
-                    nums[i] = nums[j];
-                    j++;
-                    tmp = 1;
-                }
-            }
-        }
-    }
-    return i + 1;
-}
+
 
 int main(){
 //    srand(time(NULL));
@@ -48,11 +11,12 @@ int main(){
 //    int* randomArr = generateRandomArray(genNum);
 //    Display(randomArr, genNum);
 //    testSortTime(randomArr, genNum, quickSortArr);
-    int record[] = {1, 1, 1};
-    int nums = removeDuplicates(&record, 3);
-    for(int i = 0; i < nums; i++){
-        printf("%d ", record[i]);
+//    int record[] = {1, 1, 1};
+//    int nums = removeDuplicates(&record, 3);
+    int tmp[5][5] = {0};
+    for(int i = 0; i < 5; i++){
+        printf("%d ", tmp[i][i]);
     }
-    printf("%d", nums);
+//    printf("%d", nums);
     return 0;
 }
