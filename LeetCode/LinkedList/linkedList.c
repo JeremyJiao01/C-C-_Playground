23957e71c42fb0e9de03b5d5403192a96559e956
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-03-01 16:44:09 +0800

    Added # 92

--- LeetCode/LinkedList/linkedList.c
+++ LeetCode/LinkedList/linkedList.c
@@ -35,6 +35,40 @@ struct ListNode* reverseList(struct ListNode* head) {
     return newNode;
 }
 
+// # 92 翻转指定区间内的元素
+struct ListNode *reverseBetween(struct ListNode *head, int left, int right) {
+    // 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+    struct ListNode *dummyNode = malloc(sizeof(struct ListNode));
+    dummyNode->val = -1;
+    dummyNode->next = head;
 
+    struct ListNode *pre = dummyNode;
+    // 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+    for (int i = 0; i < left - 1; i++) {
+        pre = pre->next;
+    }
+
+    // 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+    struct ListNode *rightNode = pre;
+    for (int i = 0; i < right - left + 1; i++) {
+        rightNode = rightNode->next;
+    }
+
+    // 第 3 步：切断出一个子链表（截取链表）
+    struct ListNode *leftNode = pre->next;
+    struct ListNode *curr = rightNode->next;
+
+    // 注意：切断链接
+    pre->next = NULL;
+    rightNode->next = NULL;
+
+    // 第 4 步：同第 206 题，反转链表的子区间
+    reverseLinkedList(leftNode);
+
+    // 第 5 步：接回到原来的链表中
+    pre->next = rightNode;
+    leftNode->next = curr;
+    return dummyNode->next;
+}
 
 
--- LeetCode/Tools/Tools.c
+++ LeetCode/Tools/Tools.c
@@ -4,36 +4,8 @@
 
 #include "Func.h"
 
-int maxArea(int* height, int heightSize) {
-    if(heightSize < 3){
-        return 0;
-    }
-    int leftMax = height[0];
-    int rightMax = height[heightSize - 1];
-    int S = 0;
-    int ans = 0;
-    int left = 0;
-    int right = heightSize - 1;
-    while(left <= right){
-        int minVal = leftMax < rightMax ? leftMax : rightMax;
-        if(minVal == leftMax){
-            if(height[left] >= leftMax){
-                S = (right - left) * leftMax;
-                ans = S > ans ? S : ans;
-            }
-            left++;
-            leftMax = height[left] > leftMax ? height[left] : leftMax;
-        }else{
-            if(height[right] >= rightMax){
-                S = (right - left) * rightMax;
-                ans = S > ans ? S : ans;
-            }
-            right--;
-            rightMax = height[right] > rightMax ? height[right] : rightMax;
-        }
-    }
-    return ans;
-}
+
+
 
 int main(){
 //    srand(time(NULL));
@@ -41,13 +13,16 @@ int main(){
 //    int* randomArr = generateRandomArray(genNum);
 //    Display(randomArr, genNum);
 //    testSortTime(randomArr, genNum, quickSortArr);
-    int record[] = {1,8,6,2,5,4,8,3,7};
-    int nums = maxArea(record, 9);
+//    int record[] = {1,8,6,2,5,4,8,3,7};
+//    int nums = maxArea(record, 9);
 //    int tmp[5][5] = {0};
-//    for(int i = 0; i < 5; i++){
-//        printf("%d ", tmp[i][i]);
-//    }
-    printf("%d", nums);
-
+    for(int i = 0; i < 3; i++){
+        printf("%d ", i);
+    }
+//    printf("%d", nums);
+    NODE L;
+    initnode(&L, 0);
+    CreatList(L, 6);
+    PrintList(L);
     return 0;
 }
