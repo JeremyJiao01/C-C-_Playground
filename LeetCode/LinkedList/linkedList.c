7f58442e0fb61da6fd556509616b93235c6f6173
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-03-10 11:06:55 +0800

    Added # 2

--- LeetCode/LinkedList/linkedList.c
+++ LeetCode/LinkedList/linkedList.c
@@ -365,3 +365,42 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
     deleteNode->next = NULL;
     return dummyNode->next;
 }
+
+// # 2 两数相加
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    // 1. 两链表从头开始遍历
+    //    若遍历到一个链表中有元素，另一无元素时自动为0
+    // 2. 若两数之和 / 10 不为0，则Carry flag = 1
+    //    当遍历到下一个元素时自动+1，同时Carry flag = 0
+    //
+    struct ListNode *head = NULL, *tail = NULL;
+    int carry = 0;
+    while (l1 || l2) {
+        int n1 = l1 ? l1->val : 0;
+        int n2 = l2 ? l2->val : 0;
+        int sum = n1 + n2 + carry;
+        if (!head) {
+            head = tail = malloc(sizeof(struct ListNode));
+            tail->val = sum % 10;
+            tail->next = NULL;
+        } else {
+            tail->next = malloc(sizeof(struct ListNode));
+            tail->next->val = sum % 10;
+            tail = tail->next;
+            tail->next = NULL;
+        }
+        carry = sum / 10;
+        if (l1) {
+            l1 = l1->next;
+        }
+        if (l2) {
+            l2 = l2->next;
+        }
+    }
+    if (carry > 0) {
+        tail->next = malloc(sizeof(struct ListNode));
+        tail->next->val = carry;
+        tail->next->next = NULL;
+    }
+    return head;
+}
