ee25e9f84ee02b6216d161df33c1178d681e4306
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-03-27 09:56:05 +0800

    Added # 1019

--- LeetCode/Stack/Stack.c
+++ LeetCode/Stack/Stack.c
@@ -342,6 +342,49 @@ int largestRectangleArea(int* heights, int heightsSize) {
     return res;
 }
 
+// # 1019 链表中的下一个更大节点
+typedef struct Pair {
+    // 将栈保存为两个元素，第一个保存值，第二个保存索引
+    int first;
+    int second;
+} Pair;
+
+int* nextLargerNodes(struct ListNode* head, int* returnSize) {
+    int len = 0;
+    struct ListNode* cur = head;
+    while (cur) {
+        // 计算链表长度
+        cur = cur->next;
+        len++;
+    }
+    int* ans = (int *)calloc(len, sizeof(int));
+    Pair stack[len];
+    int top = 0, pos = 0;
+
+    cur = head;
+    int idx = -1;
+    while (cur) {
+        ++idx;
+        ans[pos++] = 0;
+        while (top > 0 && stack[top - 1].first < cur->val) {
+            ans[stack[top - 1].second] = cur->val;
+            top--;
+        }
+        stack[top].first = cur->val;
+        stack[top].second = idx;
+        top++;
+        cur = cur->next;
+    }
+    *returnSize = len;
+    return ans;
+}
+
+
+
+
+
+
+
 
 
 
