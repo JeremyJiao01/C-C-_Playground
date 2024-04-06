ba1b8b7a366bb45b5ba37eec16b5b5ac5df5d310
JeremyJiao01 <JeremyJiao1107@gmail.com>
2024-04-06 15:49:47 +0800

    Added # 239

--- LeetCode/Queue/Queue.c
+++ LeetCode/Queue/Queue.c
@@ -66,4 +66,37 @@ void myCircularQueueFree(MyCircularQueue* obj) {
     obj = NULL;
 }
 
+// # 239 滑动窗口最大值
+// 使用双端队列，实现单调队列
+int* maxSlidingWindow(int* nums, int numsSize, int k) {
+    int q[numsSize];
+    int left = 0, right = 0;
+    for (int i = 0; i < k; ++i) {
+        // 找到窗口初始位置的最大值
+        while (left < right && nums[i] >= nums[q[right - 1]]) {
+            right--;
+        }
+        q[right++] = i;
+    }
+    int returnSize = 0;
+    int* ans = malloc(sizeof(int) * (numsSize - k + 1));
+    ans[(returnSize)++] = nums[q[left]];
+    for (int i = k; i < numsSize; ++i) {
+        while (left < right && nums[i] >= nums[q[right - 1]]) {
+            // 只需找到最大值即可，是否超出滑动窗口由下一个循环来判断
+            right--;
+        }
+        q[right++] = i;
+        while (q[left] <= i - k) {
+            // 这里为什么是 (i - k) ，因为滑动窗口的原因
+            // i - k，指向的是当前 i 所在窗口的第一个元素
+            // 若q[left]（元素下标） 小于等于 窗口的第一个元素下标
+            // 代表着当前q[left]（最大值），已经不在窗口内了，需要找到下一个最大值
+            left++;
+        }
+        ans[(returnSize)++] = nums[q[left]];
+    }
+    return ans;
+}
+
 
--- LeetCode/Tools/Tools.c
+++ LeetCode/Tools/Tools.c
@@ -3,131 +3,54 @@
 //
 
 #include "Func.h"
-
-#define LEN 20
-typedef struct queue {
-    int *data;
-    int head;
-    int rear;
-    int size;
-} Queue;
-
-typedef struct {
-    Queue *queue1, *queue2;
-} MyStack;
-
-Queue *initQueue(int k) {
-    Queue *obj = (Queue *)malloc(sizeof(Queue));
-    obj->data = (int *)malloc(k * sizeof(int));
-    obj->head = -1;
-    obj->rear = -1;
-    obj->size = k;
-    return obj;
-}
-
-void enQueue(Queue *obj, int e) {
-    if (obj->head == -1) {
-        obj->head = 0;
-    }
-    obj->rear = (obj->rear + 1) % obj->size;
-    obj->data[obj->rear] = e;
-}
-
-int deQueue(Queue *obj) {
-    int a = obj->data[obj->head];
-    if (obj->head == obj->rear) {
-        obj->rear = -1;
-        obj->head = -1;
-        return a;
-    }
-    obj->head = (obj->head + 1) % obj->size;
-    return a;
-}
-
-int isEmpty(Queue *obj) {
-    return obj->head == -1;
-}
-
-MyStack *myStackCreate() {
-    MyStack *obj = (MyStack *)malloc(sizeof(MyStack));
-    obj->queue1 = initQueue(LEN);
-    obj->queue2 = initQueue(LEN);
-    return obj;
-}
-
-void myStackPush(MyStack *obj, int x) {
-    if (isEmpty(obj->queue1)) {
-        enQueue(obj->queue2, x);
-    } else {
-        enQueue(obj->queue1, x);
-    }
-}
-
-int myStackPop(MyStack *obj) {
-    if (isEmpty(obj->queue1)) {
-        while (obj->queue2->head != obj->queue2->rear) {
-            enQueue(obj->queue1, deQueue(obj->queue2));
+// # 239 滑动窗口最大值
+// 使用双端队列，实现单调队列
+int* maxSlidingWindow(int* nums, int numsSize, int k) {
+    int q[numsSize];
+    int left = 0, right = 0;
+    for (int i = 0; i < k; ++i) {
+        // 找到窗口初始位置的最大值
+        while (left < right && nums[i] >= nums[q[right - 1]]) {
+            right--;
         }
-        return deQueue(obj->queue2);
+        q[right++] = i;
     }
-    while (obj->queue1->head != obj->queue1->rear) {
-        enQueue(obj->queue2, deQueue(obj->queue1));
+    int returnSize = 0;
+    int* ans = malloc(sizeof(int) * (numsSize - k + 1));
+    ans[(returnSize)++] = nums[q[left]];
+    for (int i = k; i < numsSize; ++i) {
+        while (left < right && nums[i] >= nums[q[right - 1]]) {
+            // 只需找到最大值即可，是否超出滑动窗口由下一个循环来判断
+            right--;
+        }
+        q[right++] = i;
+        while (q[left] <= i - k) {
+            // 这里为什么是 (i - k) ，因为滑动窗口的原因
+            // i - k，指向的是当前 i 所在窗口的第一个元素
+            // 若q[left]（元素下标） 小于等于 窗口的第一个元素下标
+            // 代表着当前q[left]（最大值），已经不在窗口内了，需要找到下一个最大值
+            left++;
+        }
+        ans[(returnSize)++] = nums[q[left]];
     }
-    return deQueue(obj->queue1);
+    return ans;
 }
 
-int myStackTop(MyStack *obj) {
-    if (isEmpty(obj->queue1)) {
-        return obj->queue2->data[obj->queue2->rear];
-    }
-    return obj->queue1->data[obj->queue1->rear];
-}
-
-bool myStackEmpty(MyStack *obj) {
-    if (obj->queue1->head == -1 && obj->queue2->head == -1) {
-        return true;
-    }
-    return false;
-}
-
-void myStackFree(MyStack *obj) {
-    free(obj->queue1->data);
-    obj->queue1->data = NULL;
-    free(obj->queue1);
-    obj->queue1 = NULL;
-    free(obj->queue2->data);
-    obj->queue2->data = NULL;
-    free(obj->queue2);
-    obj->queue2 = NULL;
-    free(obj);
-    obj = NULL;
-}
 
 int main(){
-    MyStack* stack = myStackCreate();
-    myStackPush(stack, 2);
-    myStackPush(stack, 3);
-    myStackPush(stack, 4);
-    myStackPop(stack);
-    myStackPop(stack);
-    return 0;
-}
+    srand(time(NULL));
 
-//int main(){
-//    srand(time(NULL));
-//
 //    int* randomArr = generateRandomArray(genNum);
 //    Display(randomArr, genNum);
 //    testSortTime(randomArr, genNum, quickSortArr);
-//    int record[] = {1,8,6,2,5,4,8,3,7};
-//    int nums = maxArea(record, 9);
+    int record[] = {1,3,-1,-3,5,3,6,7};
+    int* returnNums = 0;
+    int* nums = maxSlidingWindow(record, 8, 3);
 //    int tmp[5][5] = {0};
-//    for(int i = 0; i < 3; i++){
-//        printf("%d ", i);
-//    }
+    for(int i = 0; i < 8; i++){
+        printf("%d ", nums[i]);
+    }
 //    printf("%d", nums);
 
-
-    
-//    return 0;
-//}
+    return 0;
+}
